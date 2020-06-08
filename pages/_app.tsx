0cda6eb40f57a31c5bbc3c8fac0ab30ee0b4aa87
Maxim Remizov <maxim_remizov@epam.com>
2020-06-08 14:52:45 +0300

    feat: Include mongoose for connect DB

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app'
 import '../styles/common.css'
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(`${process.env.REACT_APP_DB_CONNECT}`, {useNewUrlParser: true, useUnifiedTopology: true});
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
     return <Component { ...pageProps } />
